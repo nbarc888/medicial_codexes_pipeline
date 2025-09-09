5ea2118a0fd12fad0d514dcc5a65738ed6bd9f20
Nina Barcelon <nbarc888@gmail.com>
2025-09-09 15:03:35 -0400

    ICD WHO FUNCTIONAL

--- scripts/icd10cm_who_processory.py
+++ scripts/icd10cm_who_processory.py
@@ -2,17 +2,31 @@ import pandas as pd
 
 file_path = 'input\icd102019syst_codes.txt'
 
-columns = ['level', 'type', 'usage', 'sort', 'parent', 'code', 'display_code', 
-           'icd10_code', 'title_en', 'parent_title', 'detailed_title', 
+###columns = ['level', 'type', 'usage', 'sort', 'parent', 'code', 'display_code', 
+##           'icd10_code', 'title_en', 'parent_title', 'detailed_title', 
+##           'definition', 'mortality_code', 'morbidity_code1', 'morbidity_code2',
+##           'morbidity_code3', 'morbidity_code4']
+
+columns = [
+           'level', 'type', 'usage', 'sort', 'parent', 'code', 'display_code', 
+          'icd10_code', 'title_en', 'parent_title', 'detailed_title', 
            'definition', 'mortality_code', 'morbidity_code1', 'morbidity_code2',
-           'morbidity_code3', 'morbidity_code4']
+           'morbidity_code3', 'morbidity_code4' ]
+
+header = [
+'icd10_code', 'detailed_title', 'definition'
+]
 
 df = pd.read_csv(file_path, sep=';', header=None, names=columns)
 
 output_path = ('output/icd102019syst_codes.csv')
-df.to_csv(output_path, index=False)
+df.to_csv(output_path, columns = header, index=False)
 
-print(f"Successfully parsed {len(df)} records from {file_path}")
-print(f"Saved to {output_path}")
-print(f"\nFirst 5 rows:")
-print(df.head())
+### CONFIRMED FUNCTIONAL !!!!!
+
+
+##print(f"Successfully parsed {len(df)} records from {file_path}")
+##print(f"Saved to {output_path}")
+##print(f"\nFirst 5 rows:")
+##print(df.head())
+
