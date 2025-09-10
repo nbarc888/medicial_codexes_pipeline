a8aa5a84aa9037c3c5aeacb3be38d74240d8b363
Nina Barcelon <nbarc888@gmail.com>
2025-09-10 02:34:39 -0400

    ICD US to be fixed and trouble shooted

--- scripts/icd10cm_US_processor.py
+++ scripts/icd10cm_US_processor.py
@@ -11,8 +11,27 @@ with open(file_path, 'r', ecoding='utf-8') as file:
     line = line.rstrip('\n\r')
 #### use note++ to look at data stuff :3 consider using claude 
 if len(line) < 15:
-continue 
+    continue
 
 order_num = kine[0:5].strip()
 code = line[6:13].strip()
 level = line[14:15].strip()
+
+remaining_text = line[16:]
+
+parts = re.split(r'\s{4,}', remaining_text, 1)
+
+description = parts[0].strip() if len(parts) > 0 else ""
+description_detailed = parts[1].strip() if len(parts) > 1 else ""
+
+codes.append({
+            'order_num': order_num,
+            'code': code,
+            'level': level,
+            'description': description,
+            'description_detailed': description_detailed
+        })
+
+icdcodes = pd.DataFrame(codes)
+
+icdcodes.to_csv("output/icd10cm_order_2025.csv", index=False)
