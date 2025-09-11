5f25fa3cfa9a0ebcb3f302c5c7f2235e1f0e7409
Nina Barcelon <nbarc888@gmail.com>
2025-09-10 23:35:00 -0400

    moved npi old file into appropriate folder

--- utils/common_functions.py
+++ utils/common_functions.py
@@ -1,12 +1,7 @@
-### import utils.common_functions
-### a = 10
-### b = 5
-### c = utils.common_functions.substract(a, b)
-### print(c)
-
-### save_to_formats(df, base_filename)
-
-
 import pandas as pd 
-from pathlib import pathlib
-save_to_csv(module1, 'module1.csv', headers  )
+  
+# Replace 'output_file.csv' with the actual path to the CSV file
+
+
+df_short_file = pd.read_csv('output_file', nrows=50)
+print(df_short_file)
